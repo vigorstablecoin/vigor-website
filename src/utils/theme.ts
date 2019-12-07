de0bdd543063379df047bfd91a042c85cbaa48d0
laubsauger <laubsauger@gmail.com>
2019-12-07 12:35:13 +0100

    added a few colors from palette

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -4,8 +4,14 @@ const bg = `#13161E`;
 const bgLight = `#1E212E`;
 const bgLighter = `#2F3347`;
 const bgLightest = `#50587A`;
+
 const primary = `#4468CC`;
 const secondary = `#6DE4F0`;
+const tertiary = `#009AFF`;
+
+const success = `#4ECCAE`;
+const error = `#E14852`;
+const warning = `#F6D051`;
 
 const colors = {
   bg,
@@ -15,6 +21,10 @@ const colors = {
   white,
   primary,
   secondary,
+  tertiary,
+  success,
+  error,
+  warning,
 };
 
 const theme = {
