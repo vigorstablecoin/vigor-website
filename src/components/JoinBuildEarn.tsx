a7e3fb7a9b6c5fd32c5e4e87698099ecee92fc58
Jesus Garcia <jesus@venzee.com>
2019-12-11 00:22:52 -0600

    Add Join, Build. Earn Section
    This Commit add the new component for Join.Build. Earn. Section

---
+++ src/components/JoinBuildEarn.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const JumbotronWrapper = styled.div`
+  margin-top:168px;
+  padding: 16px 16%;
+  text-align: center;
+`;
+
+const JumbotronTitle = styled.h1`
+  text-transform: uppercase;
+  color: ${props => props.theme.colors.white};
+  font-size: 56px;
+  font-weight: 700;
+  margin: 16px 0;
+`;
+
+const JumbotronTitleSeparator = styled.div`
+  width: 81px;
+  height: 2px;
+  left: 48%;
+  position: relative;
+  background-color: ${props => props.theme.colors.secondary};
+`;
+
+const JumbotronContent = styled.p`
+  font-size: 18px;
+  line-height: 24px;
+  font-weight: 400;
+  margin: 16px 0 51px 0;
+  color: ${props => props.theme.colors.primary};
+`;
+
+// @todo: this should come from a global base button style
+const JumbotronLinkWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  a {
+    padding: 11px 50px;
+    border-radius: 4px;
+    background-color: ${props => props.theme.colors.primary};
+    color: ${props => props.theme.colors.white};
+    text-decoration: none;
+    font-size: 14px;
+  }
+`;
+
+type Props = {
+  title:    string,
+  subtitle: string,
+  content:  React.ReactNode,
+  link:     React.ReactNode,
+};
+
+const Hero: React.FC<Props> = props => {
+  const { title, subtitle, content, link } = props;
+
+  return (
+    <JumbotronWrapper>
+      <JumbotronTitle>{ title }</JumbotronTitle>
+      <JumbotronTitleSeparator/>
+      <JumbotronContent>{ content }</JumbotronContent>
+      <JumbotronLinkWrap>{ link }</JumbotronLinkWrap>
+    </JumbotronWrapper>
+  );
+};
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useTranslation } from "react-i18next";
 import Hero from "../components/Hero";
 import LocalizedLink from "../components/LocalizedLink";
+import JoinBuildEarn from "../components/JoinBuildEarn";
 
 const Index: React.FC<{}> = (props) => {
   const { t } = useTranslation();
@@ -18,6 +19,12 @@ const Index: React.FC<{}> = (props) => {
           </React.Fragment>
         }
         link={ <LocalizedLink to={ '/community' }>{ t(`Get Involved`) }</LocalizedLink> }
+      />
+
+      <JoinBuildEarn
+        title="Join. Build. Earn."
+        content="The Vigor Stablecoin project is governed by a DAC, (Decentralized Autonomous Community) whose goal is to build and deploy the Vigor Project based on the specifications of the Vigor Whitepaper."
+        link={ <LocalizedLink to={ '/community' }>{ t(`Get Involved`) }</LocalizedLink> } 
       />
     </React.Fragment>
   )
